331dcb7fa8dbcce82548604c2f2ac533eaf6948e
sg976 <sg9769356@gmail.com>
2022-10-08 01:17:49 +0530

    Errors handled

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -16,7 +16,7 @@ const getTask = asyncWrapper(async (req, res, next) => {
         const { id: taskID } = req.params
         const task = await Task.findOne({ _id: taskID })
         if (!task) {
-            return next(createCustomError(`No task with message ${taskID}`) , 404)
+            return next(createCustomError(`No task with message ${taskID}`, 404))
         }
         res.status(200).json({ task })
 })
@@ -24,7 +24,7 @@ const deleteTask = asyncWrapper(async (req, res, next) => {
         const { id: taskID } = req.params
         const task = await Task.findOneAndDelete({ _id: taskID })
         if (!task) {
-            return next(createCustomError(`No task with message ${taskID}`) , 404)
+            return next(createCustomError(`No task with message ${taskID}`, 404))
         }
         res.status(200).json({ task })
 })
@@ -36,7 +36,7 @@ const updateTask = asyncWrapper(async (req, res,next) => {
             runValidators: true
         })
         if (!task) {
-            return next(createCustomError(`No task with message ${taskID}`) , 404)
+            return next(createCustomError(`No task with message ${taskID}`, 404))
         }
         res.status(200).json({ task })
 })
